77a49d5a5740b1d9cae08a39d47cecaa68adde7e
nkh97430@gmail.com <nkh97430@gmail.com>
2026-01-08 16:43:20 +0100

    update Jadopte.tsx

---
+++ src/pages/Jadopte.tsx
@@ -0,0 +1,22 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+export default function Jadopte() {
+    return (
+        <>
+            <Header />
+
+            <main>
+                {/* Contenu spécifique de la page Jadopte */}
+                <h1>J’adopte</h1>
+                <p>Bienvenue sur la page Jadopte.</p>
+            </main>
+
+            <Footer />
+        </>
+
+
+        
+    );
+}
