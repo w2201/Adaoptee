9332a02d27c3a39c70c5e324ccb60e73e3114c95
nkh97430@gmail.com <nkh97430@gmail.com>
2026-01-13 10:55:30 +0100

    card animal

---
+++ src/Pages/Jadopte.tsx
@@ -0,0 +1,10 @@
+import AnimalCard from "../components/AnimalCard";
+
+export default function Jadopte () {
+return (
+    <div>
+        <h1>J'adopte :</h1>
+        <AnimalCard /> 
+    </div>
+)
+};
---
+++ src/components/AnimalCard.tsx
@@ -0,0 +1,31 @@
+
+
+export default function AnimalCard() {
+  return (
+    <div className="animal-card">
+      <img
+        src="imageUrl"
+        alt="Animal"
+        className="animal-image"
+      />
+
+      <div className="animal-content">
+        <p className="animal-type">{'{type}'}</p>
+
+        <h2 className="animal-name">{'{name}'}</h2>
+
+        <p className="animal-details">
+          {'{breed}'} · {'{age}'}
+        </p>
+
+        <p className="animal-location">{'{localisation}'}</p>
+
+        <p className="animal-description">{'{description}'}</p>
+
+        <button className="animal-button">
+          Rencontrer
+        </button>
+      </div>
+    </div>
+  );
+}
