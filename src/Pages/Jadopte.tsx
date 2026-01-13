ad0ac61589664a6f533f3681dbaaa9f71489c37a
Alison Debaille <adebaille@gmail.com>
2026-01-13 18:57:31 +0100

    modif request sql

--- src/Pages/Jadopte.tsx
+++ src/Pages/Jadopte.tsx
@@ -7,10 +7,11 @@ import { useQuery } from "@tanstack/react-query";
 export default function Jadopte () {
      interface type {
     id: number;
+    type: string;
     name: string;
-    breed_id: number;
+    breed: string;
     age: number;
-    shelter_id: number;
+    shelter: string;
     description: string
   }
 
@@ -21,10 +22,11 @@ export default function Jadopte () {
 
     console.log(type, city);
 
-    const {data: response, isLoading, error} = useQuery({
+    const {data : response, isLoading, error} = useQuery({
         queryKey: ["animals"],
         queryFn: async () => {
-        const result = await sql("SELECT * FROM animals");
+        const result = await sql(`SELECT animals.*, b.name as breed, shelters.city as shelter, types.name as type FROM animals JOIN breeds as b ON animals.breed_id = b.id JOIN shelters ON animals.shelter_id = shelters.id JOIN types ON b.type_id = types.id`);
+        // if ()
         if (!result.success) throw new Error(result.error);
         return result.data;
         },
@@ -33,17 +35,15 @@ export default function Jadopte () {
      if (isLoading) return <p>Chargement...</p>;
      if (error) return <p>Erreur : {error.message}</p>;
 
-     console.log(response)
-
     return (
         <>
-        <p>FILTER</p>
         <SearchBar />
-        {response.map((el : type) => (<div key={el.name}>
+        {response.map((el : type) => (<div key={el.name} style={{borderStyle: 'solid'}}>
+            <p id='type'>{el.type}</p>
             <p id='name'>{el.name}</p>
-            <p id='breed'>{el.breed_id}</p>
+            <p id='breed'>{el.breed}</p>
             <p id='age'>{el.age}</p>
-            <p id='localisation'>{el.shelter_id}</p>
+            <p id='localisation'>{el.shelter}</p>
             <p id='description'>{el.description}</p>
             </div>))}
         </>
